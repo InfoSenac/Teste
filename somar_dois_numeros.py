0ac3c189b7db8e46a1e077565652e5ee21106f58
Temp Account <woriwi2035@laserlip.com>
2023-02-14 17:17:59 -0300

    Criação do código

--- somar_dois_numeros.py
+++ somar_dois_numeros.py
@@ -1,5 +1,10 @@
 def somar(x, y):
     return x + y
 
+num1 = int(input('Informe o primeiro número:'))
+num2 = int(input('Informe o segundo número: '))
 
-print(somar(10, 5))
+
+
+
+print(somar(num1, num2))
